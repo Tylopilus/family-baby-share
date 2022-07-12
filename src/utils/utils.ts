3c0441689f09652768b4aaba8c11c361158fa099
Helge <elitaer@gmail.com>
2022-07-12 12:10:47 +0200

    refactor

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -3,3 +3,28 @@ export function generateHash() {
   const hash = crypto.randomBytes(20).toString('hex');
   return hash;
 }
+export async function fileToBase64(file: File): Promise<string | ArrayBuffer> {
+  return new Promise((resolve, reject) => {
+    if (!file.type.match('image')) {
+      return reject(new Error('INVALID_FILE'));
+    }
+
+    if (
+      !file.type.match('jpeg') &&
+      !file.type.match('jpg') &&
+      !file.type.match('png')
+    ) {
+      return reject(new Error('INVALID_FILE'));
+    }
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onloadend = function () {
+      const base64data = reader.result;
+      if (base64data) {
+        resolve(base64data);
+      } else {
+        reject(new Error('Unkown error'));
+      }
+    };
+  });
+}
