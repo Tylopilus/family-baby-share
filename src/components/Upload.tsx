fca584c19958f2463c41966d38f639b55b1d6c5a
Helge <elitaer@gmail.com>
2022-07-22 12:14:04 +0200

    feat: implement upload view

---
+++ src/components/Upload.tsx
@@ -0,0 +1,98 @@
+import { createSignal, Show } from 'solid-js';
+import Button from './Shared/Button';
+import { UploadIndicator } from './Shared/Icons';
+
+const Upload = () => {
+  const [selectedItems, setSelectedItems] = createSignal(0);
+  const [submitting, setSubmitting] = createSignal(false);
+  const submitHandler = async (e: Event) => {
+    const signData = await (await fetch('/api/cloudinary')).json();
+    const url =
+      'https://api.cloudinary.com/v1_1/' + signData.cloudname + '/auto/upload';
+
+    const files = (document.querySelector('[type=file]') as HTMLInputElement)!
+      .files!;
+    const formData = new FormData();
+
+    // Append parameters to the form data. The parameters that are signed using
+    // the signing function (signuploadform) need to match these.
+    for (const file of files) {
+      formData.append('file', file);
+      formData.append('api_key', signData.apikey);
+      formData.append('timestamp', signData.timestamp);
+      formData.append('signature', signData.signature);
+      formData.append('folder', signData.folder);
+      fetch(url, {
+        method: 'POST',
+        body: formData,
+      })
+        .then((response) => {
+          return response.text();
+        })
+        .then((data) => {
+          console.log(JSON.parse(data));
+          window.location.href = '/';
+        })
+        .catch((e) => console.error(e));
+    }
+    // console.log('submit');
+    setSubmitting(true);
+  };
+  return (
+    // <form method="post" enctype="multipart/form-data">
+    //   <label for="file">File</label>
+    //   <input type="file" name="file" multiple id="file" hidden />
+    //   <button type="submit">Submit</button>
+    // </form>
+    <div
+      class="absolute top-[50%] left-0 flex justify-center items-center flex-col w-full"
+      style={{ transform: 'translateY(-50%)' }}>
+      <Show
+        when={!submitting()}
+        fallback={
+          <div class="w-24 h-24">
+            <UploadIndicator />
+          </div>
+        }>
+        <Input selectedItems={selectedItems} />
+        <input
+          type="file"
+          name="file"
+          multiple
+          id="file"
+          hidden
+          onChange={(e) => {
+            setSelectedItems((e.target as HTMLInputElement).files?.length || 0);
+          }}
+        />
+      </Show>
+      {selectedItems() > 0 && (
+        <div class="mt-4">
+          <div>{selectedItems() > 1 ? 'Files' : 'File'} selected</div>
+          <Button variant="primary" class="mt-4" onClick={submitHandler}>
+            Upload
+          </Button>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Upload;
+const Input = (props: any) => {
+  return (
+    <label
+      for="file"
+      class="border rounded-full border-dashed border-black text-6xl w-24 h-24 flex items-center justify-center">
+      <Show
+        when={props.selectedItems() > 0}
+        fallback={
+          <svg class="w-24 h-24" viewBox="0 0 24 24">
+            <path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z" />
+          </svg>
+        }>
+        {props.selectedItems()}
+      </Show>
+    </label>
+  );
+};
