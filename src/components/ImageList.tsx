b925c54aa5c675e46259e13c0ebcc56b51b4bb63
Helge <elitaer@gmail.com>
2023-08-16 22:06:07 +0200

    first try infinity loading

--- src/components/ImageList.tsx
+++ src/components/ImageList.tsx
@@ -1,21 +1,55 @@
 import { createEffect, createSignal } from 'solid-js';
-import { isServer } from 'solid-js/web';
-import { getImage, Resources } from '../utils/cloudinary';
+import { Resources } from '../utils/cloudinary';
 
 type Props = {
   images: Resources[];
+  cursor: string | null;
 };
+let imageList: HTMLDivElement;
 const ImageList = (props: Props) => {
+  const [images, setImaages] = createSignal<Resources[]>(props.images);
+  const [cursor, setCursor] = createSignal<string | null>(props.cursor);
+  let fetching = false;
+  const handleScroll = async () => {
+    if (
+      window.innerHeight + window.scrollY >= document.body.offsetHeight &&
+      !fetching
+    ) {
+      fetching = true;
+      await fetchNextData();
+      fetching = false;
+    }
+  };
+
+  createEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+  });
+  const fetchNextData = async () => {
+    if (!cursor()) return;
+    const res = await (
+      await fetch('/api/getimage.json?cursor=' + cursor())
+    ).json();
+    setImaages([...images(), ...res.images]);
+    setCursor(res.cursor);
+  };
   return (
-    <>
-      {props.images.map((image) => (
-        <Image image={image} />
-      ))}
-    </>
+    <div ref={imageList}>
+      {images().map((image, idx) =>
+        props.images.length - 1 === idx ? (
+          <Image image={image} last={true} />
+        ) : (
+          <Image image={image} last={false} />
+        )
+      )}
+    </div>
   );
 };
 export default ImageList;
-const Image = (props: { image: Resources }) => {
+const Image = (props: {
+  image: Resources;
+  last: boolean;
+  ref?: HTMLImageElement | any;
+}) => {
   const modifiers = 'w_1000,h_1000,c_limit';
   const splitUrl = props.image.secure_url.split('/image/upload/');
   const modifierUrl =
@@ -23,13 +57,11 @@ const Image = (props: { image: Resources }) => {
   const url = modifierUrl.replaceAll(/jpg|png|jpeg/gi, 'webp');
 
   return (
-    // <div ref={ref} class="image-wrapper">
-    //   <img src={props.image.secure_url} alt={props.image.public_id} />
-    // </div>
     <img
+      ref={props.ref}
       src={url}
       alt={props.image.public_id}
-      loading="lazy"
+      // loading="lazy"
       width={props.image.width}
       height={props.image.height}
     />
--- src/pages/api/getImage.ts
+++
@@ -1,3 +0,0 @@
-export async function get({ request }: { request: Request }) {
-  return new Response('Not implemented', { status: 501 });
-}
